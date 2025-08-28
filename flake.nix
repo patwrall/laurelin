2409a549a7147f122d75f38d11bce9f3f49d56e7
Patrick Rall <patrickwrall@gmail.com>
2025-08-27 21:55:56 -0500

    feat(flake): extra substituters

--- flake.nix
+++ flake.nix
@@ -20,6 +20,18 @@
     nixpkgs-master.url = "github:nixos/nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://laurelin.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "laurelin.cachix.org-1:1N/DsBUemArpMoVZAl1jkosNLYBYlmLFyY7hYkuJ394="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    allow-import-from-derivation = false;
+  };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
