d28bab1ff6e9a9d48ce8ad895f1f21ed6a492177
Patrick Rall <patrickwrall@gmail.com>
2025-08-31 00:37:48 -0500

    feat(nixvim/plugins/conform): formatting with conform

---
+++ modules/nixvim/plugins/conform/default.nix
@@ -0,0 +1,249 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+{
+  plugins = {
+    conform-nvim = {
+      enable = true;
+
+      lazyLoad.settings = {
+        cmd = [
+          "ConformInfo"
+        ];
+        event = [ "BufWritePre" ];
+      };
+
+      luaConfig.pre = ''
+        local slow_format_filetypes = {}
+      '';
+
+      settings = {
+        default_format_opts = {
+          lsp_format = "fallback";
+        };
+
+        format_on_save = # Lua
+          ''
+            function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+            end
+
+            -- Disable autoformat for slow filetypes
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+            end
+
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match("/node_modules/") or bufname:match("/.direnv/") then
+            return
+            end
+
+            local function on_format(err)
+            if err and err:match("timeout$") then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+            end
+            end
+
+            return { timeout_ms = 500, lsp_format = "fallback" }, on_format
+            end
+          '';
+
+        format_after_save = # Lua
+          ''
+            function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+            end
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+            end
+
+            return { lsp_fallback = true }
+            end
+          '';
+
+        # NOTE:
+        # Conform will run multiple formatters sequentially
+        # [ "1" "2" "3"]
+        # Add stop_after_first to run only the first available formatter
+        # { "__unkeyed-1" = "foo"; "__unkeyed-2" = "bar"; stop_after_first = true; }
+        # Use the "*" filetype to run formatters on all filetypes.
+        # Use the "_" filetype to run formatters on filetypes that don't
+        # have other formatters configured.
+        formatters_by_ft = {
+          bash = [
+            "shellcheck"
+            "shellharden"
+            "shfmt"
+          ];
+          bicep = [ "bicep" ];
+          c = [ "clang_format" ];
+          cmake = [ "cmake-format" ];
+          cpp = [ "clang_format" ];
+          cs = [ "csharpier" ];
+          css = [ "stylelint" ];
+          fish = [ "fish_indent" ];
+          fsharp = [ "fantomas" ];
+          gdscript = [ "gdformat" ];
+          go = [ "golines" ];
+          java = [ "google-java-format" ];
+          javascript = {
+            __unkeyed-1 = "prettierd";
+            __unkeyed-2 = "biome";
+            timeout_ms = 2000;
+            stop_after_first = true;
+          };
+          json = [ "jq" ];
+          html = [ "prettierd" ];
+          http = [ "kulala-fmt" ];
+          kdl = [ "kdlfmt" ];
+          lua = [ "stylua" ];
+          markdown = [ "deno_fmt" ];
+          nix = [ "nixfmt" ];
+          python = [
+            "isort"
+            "ruff"
+          ];
+          rest = [ "kulala-fmt" ];
+          rust = [ "rustfmt" ];
+          sh = [
+            "shellcheck"
+            "shellharden"
+            "shfmt"
+          ];
+          sql = [ "sqlfluff" ];
+          swift = [ "swift_format" ];
+          terraform = [ "terraform_fmt" ];
+          toml = [ "taplo" ];
+          typescript = {
+            __unkeyed-1 = "prettierd";
+            __unkeyed-2 = "biome";
+            timeout_ms = 2000;
+            stop_after_first = true;
+          };
+          xml = [
+            "xmlformat"
+            "xmllint"
+          ];
+          yaml = [ "yamlfmt" ];
+          zig = [ "zigfmt" ];
+          "_" = [
+            "squeeze_blanks"
+            "trim_whitespace"
+            "trim_newlines"
+          ];
+        };
+
+        formatters = {
+          bicep.command = lib.getExe pkgs.bicep;
+          biome = {
+            command = lib.getExe pkgs.biome;
+            env = {
+              BIOME_CONFIG_PATH = pkgs.writeTextFile {
+                name = "biome.json";
+                text = lib.generators.toJSON { } {
+                  "$schema" = "${pkgs.biome}/node_modules/@biomejs/biome/configuration_schema.json";
+                  formatter.useEditorconfig = true;
+                };
+              };
+            };
+          };
+          black.command = lib.getExe pkgs.black;
+          cmake-format.command = lib.getExe pkgs.cmake-format;
+          csharpier.command = lib.getExe pkgs.csharpier;
+          deno_fmt.command = lib.getExe pkgs.deno;
+          fantomas.command = lib.getExe pkgs.fantomas;
+          # FIXME: broken upstream
+          # gdformat.command = lib.getExe' pkgs.gdtoolkit_4 "gdformat";
+          golines.command = lib.getExe pkgs.golines;
+          google-java-format.command = lib.getExe pkgs.google-java-format;
+          isort.command = lib.getExe pkgs.isort;
+          jq.command = lib.getExe pkgs.jq;
+          kdlfmt.command = lib.getExe pkgs.kdlfmt;
+          kulala-fmt.command = lib.getExe pkgs.kulala-fmt;
+          nixfmt.command = lib.getExe pkgs.nixfmt;
+          prettierd.command = lib.getExe pkgs.prettierd;
+          ruff.command = lib.getExe pkgs.ruff;
+          rustfmt.command = lib.getExe pkgs.rustfmt;
+          shellcheck.command = lib.getExe pkgs.shellcheck;
+          shellharden.command = lib.getExe pkgs.shellharden;
+          shfmt.command = lib.getExe pkgs.shfmt;
+          sqlfluff.command = lib.getExe pkgs.sqlfluff;
+          squeeze_blanks.command = lib.getExe' pkgs.coreutils "cat";
+          stylelint.command = lib.getExe pkgs.stylelint;
+          stylua.command = lib.getExe pkgs.stylua;
+          swift_format.command = lib.getExe pkgs.swift-format;
+          taplo.command = lib.getExe pkgs.taplo;
+          terraform_fmt.command = lib.getExe pkgs.terraform;
+          xmlformat.command = lib.getExe pkgs.xmlformat;
+          yamlfmt.command = lib.getExe pkgs.yamlfmt;
+          zigfmt.command = lib.getExe pkgs.zig;
+        };
+      };
+    };
+  };
+
+  keymaps = lib.optionals config.plugins.conform-nvim.enable [
+    {
+      action.__raw = ''
+        function(args)
+        vim.cmd({cmd = 'Conform', args = args});
+        end
+      '';
+      mode = "v";
+      key = "<leader>cf";
+      options = {
+        silent = true;
+        buffer = false;
+        desc = "Format selection";
+      };
+    }
+    {
+      action.__raw = ''
+        function()
+        vim.cmd('Conform');
+        end
+      '';
+      key = "<leader>cf";
+      options = {
+        silent = true;
+        desc = "Format buffer";
+      };
+    }
+  ];
+
+  userCommands = lib.mkIf config.plugins.conform-nvim.enable {
+    Conform = {
+      desc = "Formatting using conform.nvim";
+      range = true;
+      command.__raw = ''
+        function(args)
+        ${lib.optionalString config.plugins.lz-n.enable "require('lz.n').trigger_load('conform.nvim')"}
+        local range = nil
+        if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+        start = { args.line1, 0 },
+        ["end"] = { args.line2, end_line:len() },
+        }
+        end
+        require("conform").format({ async = true, lsp_format = "fallback", range = range },
+        function(err)
+        if not err then
+        local mode = vim.api.nvim_get_mode().mode
+        if vim.startswith(string.lower(mode), "v") then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+        end
+        end
+        end)
+        end
+      '';
+    };
+  };
+}
+
