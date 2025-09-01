1efadb4454b06edd3d271dad2055301fdfb2c5b6
Patrick Rall <patrickwrall@gmail.com>
2025-09-01 18:22:27 -0500

    feat(nixvim/plugins/clangd-extensions): clangd extensions

---
+++ modules/nixvim/plugins/clangd-extensions/default.nix
@@ -0,0 +1,41 @@
+{ config
+, ...
+}:
+{
+  plugins = {
+    clangd-extensions = {
+      lazyLoad.settings.cmd = [
+        "ClangdAST"
+        "ClangdTypeHierarchy"
+        "ClangdSymbolInfo"
+        "ClangdMemoryUsage"
+        "ClangdSwitchSourceHeader"
+      ];
+
+      inherit (config.plugins.treesitter) enable;
+      enableOffsetEncodingWorkaround = true;
+
+      settings = {
+        ast = {
+          roleIcons = {
+            type = "";
+            declaration = "";
+            expression = "";
+            specifier = "";
+            statement = "";
+            templateArgument = "";
+          };
+          kindIcons = {
+            compound = "";
+            recovery = "";
+            translationUnit = "";
+            packExpansion = "";
+            templateTypeParm = "";
+            templateTemplateParm = "";
+            templateParamObject = "";
+          };
+        };
+      };
+    };
+  };
+}
