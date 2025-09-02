67c2384930a7dcaf47e83a2048600e681df0dcd2
Patrick Rall <patrickwrall@gmail.com>
2025-09-02 14:18:08 -0500

    feat(nixvim/plugins): dap stuff

---
+++ modules/nixvim/plugins/dap-virtual-text/default.nix
@@ -0,0 +1,24 @@
+{ config
+, lib
+, ...
+}:
+{
+  plugins = {
+    dap-virtual-text = {
+      enable = true;
+
+      lazyLoad.settings = {
+        before.__raw = lib.mkIf config.plugins.lz-n.enable ''
+          function()
+            require('lz.n').trigger_load('nvim-dap')
+          end
+        '';
+        cmd = [
+          "DapVirtualTextEnable"
+          "DapVirtualTextForceRefresh"
+          "DapVirtualTextToggle"
+        ];
+      };
+    };
+  };
+}
