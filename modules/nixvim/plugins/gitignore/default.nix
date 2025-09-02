b24e73c4085caee78904df4b4c0770eb0c5b1ec1
Patrick Rall <patrickwrall@gmail.com>
2025-09-02 14:44:59 -0500

    feat(nixvim/plugins/gitignore): gitignore module

---
+++ modules/nixvim/plugins/gitignore/default.nix
@@ -0,0 +1,30 @@
+{ config
+, lib
+, ...
+}:
+let
+  cfg = config.plugins.gitignore;
+in
+{
+  plugins = {
+    gitignore = {
+      enable = true;
+
+      # TODO: migrate to mkNeovimPlugin
+      # lazyLoad.settings.keys = [ "<leader>gi" ];
+      # lazyLoad.settings.cmd = [ "Gitignore" ];
+    };
+  };
+
+  keymaps = lib.mkIf cfg.enable [
+    {
+      mode = "n";
+      key = "<leader>gi";
+      action.__raw = ''require('gitignore').generate'';
+      options = {
+        desc = "Gitignore generate";
+        silent = true;
+      };
+    }
+  ];
+}
