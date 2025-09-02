0e2024c54e7ab5fd9aed19ebca59e632d32b8837
Patrick Rall <patrickwrall@gmail.com>
2025-09-02 16:37:30 -0500

    feat(nixvim/plugins/precognition): precognition

---
+++ modules/nixvim/plugins/precognition/default.nix
@@ -0,0 +1,34 @@
+{ lib, config, ... }:
+{
+  plugins.precognition = {
+    enable = true;
+
+    lazyLoad.settings.cmd = "Precognition";
+
+    settings = {
+      startVisible = false;
+    };
+  };
+
+  keymaps = lib.optionals config.plugins.precognition.enable [
+    {
+      mode = "n";
+      key = "<leader>vp";
+      action.__raw = ''
+        function()
+          local toggled = vim.cmd("Precognition toggle")
+          if toggled then
+              vim.notify("Precognition on")
+          else
+              vim.notify("Precognition off")
+          end
+        end
+      '';
+
+      options = {
+        desc = "Precognition Toggle";
+        silent = true;
+      };
+    }
+  ];
+}
