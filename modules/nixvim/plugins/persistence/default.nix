fbf9e0bf9421eaf4d3d38beba3651e588b69a491
Patrick Rall <patrickwrall@gmail.com>
2025-09-02 16:28:16 -0500

    feat(nixvim/plugins/persistence): session management

---
+++ modules/nixvim/plugins/persistence/default.nix
@@ -0,0 +1,48 @@
+{ config
+, lib
+, ...
+}:
+{
+  plugins = {
+    persistence = {
+      enable = true;
+
+      lazyLoad.settings.event = "BufReadPre";
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.persistence.enable [
+      {
+        __unkeyed-1 = "<leader>P";
+        group = "Persistence";
+        icon = "󰘛";
+      }
+    ];
+  };
+
+  keymaps = lib.optionals config.plugins.persistence.enable [
+    {
+      mode = "n";
+      key = "<leader>Pl";
+      action.__raw = "function() require('persistence').load() end";
+      options.desc = "Load the session for the current directory";
+    }
+    {
+      mode = "n";
+      key = "<leader>Ps";
+      action.__raw = "function() require('persistence').select() end";
+      options.desc = "Select a session to load";
+    }
+    {
+      mode = "n";
+      key = "<leader>PL";
+      action.__raw = "function() require('persistence').load({ last = true }) end";
+      options.desc = "Load the last session";
+    }
+    {
+      mode = "n";
+      key = "<leader>PS";
+      action.__raw = "function() require('persistence').stop() end";
+      options.desc = "Stop Persistence";
+    }
+  ];
+}
