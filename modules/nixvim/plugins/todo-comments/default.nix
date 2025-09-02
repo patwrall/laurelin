38c1c5223eda47e33f9dd719b1d7536cd1d72393
Patrick Rall <patrickwrall@gmail.com>
2025-09-02 17:06:54 -0500

    feat(nixvim/plugins/trouble): trouble module

--- modules/nixvim/plugins/todo-comments/default.nix
+++ modules/nixvim/plugins/todo-comments/default.nix
@@ -22,14 +22,14 @@
           + lib.optionalString config.plugins.telescope.enable ''
             require('lz.n').trigger_load('telescope')
           ''
-          + lib.optionalString (config.telperion.picker.engine == "snacks") ''
+          + lib.optionalString (config.laurelin.picker.engine == "snacks") ''
             require('lz.n').trigger_load('snacks.nvim')
           ''
           + ''
             end
           ''
         );
-        keys = lib.mkIf (config.telperion.picker.engine == "snacks") [
+        keys = lib.mkIf (config.laurelin.picker.engine == "snacks") [
           {
             __unkeyed-1 = "<leader>ft";
             __unkeyed-2 = ''<CMD>lua Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" }})<CR>'';
