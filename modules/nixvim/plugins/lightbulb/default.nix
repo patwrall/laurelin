adf6093f0b06ccd206bf4a83fda9584fdf344d50
Patrick Rall <patrickwrall@gmail.com>
2026-04-14 20:23:34 -0400

    chore: flake.lock update

--- modules/nixvim/plugins/lightbulb/default.nix
+++ modules/nixvim/plugins/lightbulb/default.nix
@@ -1,19 +1,8 @@
-{ pkgs
-, ...
-}:
+_:
 {
   plugins = {
     nvim-lightbulb = {
       enable = true;
-      package = pkgs.vimPlugins.nvim-lightbulb.overrideAttrs (_old: {
-        patches = [
-          (pkgs.fetchpatch {
-            url = "https://github.com/kosayoda/nvim-lightbulb/pull/78.patch";
-            hash = "sha256-Ui28LmA26K+E+byP0lOspAjXjSvokoDpSW7wdnNT1xk=";
-          })
-        ];
-      });
-
       lazyLoad.settings.event = "DeferredUIEnter";
 
       settings = {
